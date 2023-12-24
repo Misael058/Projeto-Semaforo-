5fdaafab06eb1d22c4b03a3adade7ded6e9435dc
Misael sapiente <147666566+Misael058@users.noreply.github.com>
2023-12-24 15:05:11 -0300

    Add files via upload

---
+++ semaforoProjeto/semaforo.js
@@ -0,0 +1,16 @@
+const img = document.getElementById( 'img' );
+const red = document.getElementById("red");
+const yellow = document.getElementById("yellow");
+const green = document.getElementById("green");
+
+function mudouVermelho(){
+    img.src ="img/vermelho.png";
+}
+
+function mudouAmarelo(){
+    img.src ="img/amarelo.png";
+}
+
+function mudouVerde(){
+    img.src ="img/verde.png";
+}
